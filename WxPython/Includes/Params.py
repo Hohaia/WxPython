2e9903b0be441aef0fa149c55435c5ab95c9fbc1
Josiah <hohaia.code@gmail.com>
2023-05-17 20:48:31 +1200

    params update

--- WxPython/Includes/Params.py
+++ WxPython/Includes/Params.py
@@ -1,6 +1,6 @@
 # login parameters
 init = "Command&Type=Session&SubType=InitSession"
-login = f"Command&Type=Session&SubType=CheckPassword"
+login = "Command&Type=Session&SubType=CheckPassword"
 
 # query parameters
 userList = "Request&Type=List&SubType=GXT_USERS_TBL"
