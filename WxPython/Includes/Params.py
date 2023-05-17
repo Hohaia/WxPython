80e7a459a9e9913e9b84d9c6a3e6c9c07d27df61
Josiah <hohaia.code@gmail.com>
2023-05-17 22:41:10 +1200

    parameters added

--- WxPython/Includes/Params.py
+++ WxPython/Includes/Params.py
@@ -2,5 +2,20 @@
 init = "Command&Type=Session&SubType=InitSession"
 login = "Command&Type=Session&SubType=CheckPassword"
 
-# query parameters
+
+## query parameters
+
+#* TODO users
 userList = "Request&Type=List&SubType=GXT_USERS_TBL"
+
+#* TODO areas
+areaList = "Request&Type=List&SubType=GXT_AREAS_TBL"
+
+#* TODO doors
+doorList = "Request&Type=List&SubType=GXT_DOORS_TBL"
+
+#* TODO inputs
+inputList = "Request&Type=List&SubType=GXT_INPUTS_TBL"
+
+#* TODO outputs
+outputList = "Request&Type=List&SubType=GXT_PGMS_TBL"
