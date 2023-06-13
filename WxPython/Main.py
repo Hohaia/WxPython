371cc5167ed462548efee911ae7e53b0e8ce8da0
Josiah <hohaia.code@gmail.com>
2023-06-13 21:02:11 +1200

    code style updated

--- WxPython/Main.py
+++ WxPython/Main.py
@@ -26,7 +26,7 @@ class Area(WxIO):
     def __init__(self, name, recID, sessid):
         super().__init__(name, recID, sessid)
         self.statusKey = "Area" + str(self.recID)
-    def printStatus(self):
+    def print_Status(self):
         lst = self.status()[:]
         lst[0] = codeMaps.areaStatus1.get(lst[0], lst[0])
         lst[1] = codeMaps.areaStatus2.get(lst[1], lst[1])
@@ -34,95 +34,95 @@ class Area(WxIO):
         lst[2] = [codeMaps.areaStatus3.get(i, i) for i in range(len(binary_str)) if binary_str[i] == '1']
         print(f"{self.name}: {self.statusKey}{self.status()} - {lst}")
     def disarm(self):
-        runQuery(self.url, self.sessid, params.control, params.areas, self.recID, 0)
-    def disarm24(self):
-        runQuery(self.url, self.sessid, params.control, params.areas, self.recID, 1)
-    def disarmAll(self):
-        runQuery(self.url, self.sessid, params.control, params.areas, self.recID, 2)
+        run_Query(self.url, self.sessid, params.control, params.areas, self.recID, 0)
+    def disarm_24(self):
+        run_Query(self.url, self.sessid, params.control, params.areas, self.recID, 1)
+    def disarm_All(self):
+        run_Query(self.url, self.sessid, params.control, params.areas, self.recID, 2)
     def arm(self):
-        runQuery(self.url, self.sessid, params.control, params.areas, self.recID, 3)
-    def armForce(self):
-        runQuery(self.url, self.sessid, params.control, params.areas, self.recID, 4)
-    def armInstant(self):
-        runQuery(self.url, self.sessid, params.control, params.areas, self.recID, 5)
-    def armForceInstant(self):
-        runQuery(self.url, self.sessid, params.control, params.areas, self.recID, 6)
-    def walkTest_On(self):
-        runQuery(self.url, self.sessid, params.control, params.areas, self.recID, 7)
-    def walkTest_Off(self):
-        runQuery(self.url, self.sessid, params.control, params.areas, self.recID, 8)
-    def silenceAlarm(self):
-        runQuery(self.url, self.sessid, params.control, params.areas, self.recID, 9)
-    def armStay(self):
-        runQuery(self.url, self.sessid, params.control, params.areas, self.recID, 10)
-    def arm24(self):
-        runQuery(self.url, self.sessid, params.control, params.areas, self.recID, 11)
+        run_Query(self.url, self.sessid, params.control, params.areas, self.recID, 3)
+    def arm_Force(self):
+        run_Query(self.url, self.sessid, params.control, params.areas, self.recID, 4)
+    def arm_Instant(self):
+        run_Query(self.url, self.sessid, params.control, params.areas, self.recID, 5)
+    def arm_Force_Instant(self):
+        run_Query(self.url, self.sessid, params.control, params.areas, self.recID, 6)
+    def walk_Test_On(self):
+        run_Query(self.url, self.sessid, params.control, params.areas, self.recID, 7)
+    def walk_Test_Off(self):
+        run_Query(self.url, self.sessid, params.control, params.areas, self.recID, 8)
+    def silence_Alarm(self):
+        run_Query(self.url, self.sessid, params.control, params.areas, self.recID, 9)
+    def arm_Stay(self):
+        run_Query(self.url, self.sessid, params.control, params.areas, self.recID, 10)
+    def arm_24(self):
+        run_Query(self.url, self.sessid, params.control, params.areas, self.recID, 11)
 
 # subclass for door objects (inherits from wxIO)
 class Door(WxIO):
     def __init__(self, name, recID, sessid):
         super().__init__(name, recID, sessid)
         self.statusKey = "Door" + str(self.recID)
-    def printStatus(self):
+    def print_Status(self):
         lst = self.status()[:]
         lst[0] = codeMaps.doorStatus1.get(lst[0], lst[0])
         lst[1] = codeMaps.doorStatus2.get(lst[1], lst[1])
         print(f"{self.name}: {self.statusKey}{self.status()} - {lst}")
     def lock(self):
-        runQuery(self.url, self.sessid, params.control, params.doors, self.recID, 0)
+        run_Query(self.url, self.sessid, params.control, params.doors, self.recID, 0)
     def unlock(self):
-        runQuery(self.url, self.sessid, params.control, params.doors, self.recID, 1)
-    def unlockLatched(self):
-        runQuery(self.url, self.sessid, params.control, params.doors, self.recID, 2)
-    def lockDown_EntryAllowed(self):
-        runQuery(self.url, self.sessid, params.control, params.doors, self.recID, 3)
-    def lockDown_ExitAllowed(self):
-        runQuery(self.url, self.sessid, params.control, params.doors, self.recID, 4)
-    def lockDown_EntryExitAllowed(self):
-        runQuery(self.url, self.sessid, params.control, params.doors, self.recID, 5)
-    def lockDown_Clear(self):
-        runQuery(self.url, self.sessid, params.control, params.doors, self.recID, 6)
-    def cancelConditionalException(self):
-        runQuery(self.url, self.sessid, params.control, params.doors, self.recID, 7)
-    def restoreConditionalException(self):
-        runQuery(self.url, self.sessid, params.control, params.doors, self.recID, 8)
-    def lockDown_Full(self):
-        runQuery(self.url, self.sessid, params.control, params.doors, self.recID, 9)
+        run_Query(self.url, self.sessid, params.control, params.doors, self.recID, 1)
+    def unlock_Latched(self):
+        run_Query(self.url, self.sessid, params.control, params.doors, self.recID, 2)
+    def lockdown_Entry_Allowed(self):
+        run_Query(self.url, self.sessid, params.control, params.doors, self.recID, 3)
+    def lockdown_Exit_Allowed(self):
+        run_Query(self.url, self.sessid, params.control, params.doors, self.recID, 4)
+    def lockdown_Entry_Exit_Allowed(self):
+        run_Query(self.url, self.sessid, params.control, params.doors, self.recID, 5)
+    def lockdown_Clear(self):
+        run_Query(self.url, self.sessid, params.control, params.doors, self.recID, 6)
+    def cancel_Conditional_Exception(self):
+        run_Query(self.url, self.sessid, params.control, params.doors, self.recID, 7)
+    def restore_Conditional_Exception(self):
+        run_Query(self.url, self.sessid, params.control, params.doors, self.recID, 8)
+    def lockdown_Full(self):
+        run_Query(self.url, self.sessid, params.control, params.doors, self.recID, 9)
 
 # subclass for input objects (inherits from wxIO)
 class Input(WxIO):
     def __init__(self, name, recID, sessid):
         super().__init__(name, recID, sessid)
         self.statusKey = "Input" + str(self.recID)
-    def printStatus(self):
+    def print_Status(self):
         lst = self.status()[:]
         lst[0] = codeMaps.inputStatus1.get(lst[0], lst[0])
         binary_str = bin(lst[1])[2:].rjust(3, '0')
         lst[1] = [codeMaps.inputStatus2.get(i, i) for i in range(len(binary_str)) if binary_str[i] == '1']
         print(f"{self.name}: {self.statusKey}{self.status()} - {lst}")
-    def removeBypass(self):
-        runQuery(self.url, self.sessid, params.control, params.inputs, self.recID, 0)
-    def bypassTillDisarm(self):
-        runQuery(self.url, self.sessid, params.control, params.inputs, self.recID, 1)
+    def remove_Bypass(self):
+        run_Query(self.url, self.sessid, params.control, params.inputs, self.recID, 0)
+    def bypass_Till_Disarm(self):
+        run_Query(self.url, self.sessid, params.control, params.inputs, self.recID, 1)
     def bypass(self):
-        runQuery(self.url, self.sessid, params.control, params.inputs, self.recID, 2)
+        run_Query(self.url, self.sessid, params.control, params.inputs, self.recID, 2)
 
 # subclass for output objects (inherits from wxIO)
 class Output(WxIO):
     def __init__(self, name, recID, sessid):
         super().__init__(name, recID, sessid)
         self.statusKey = "PGM" + str(self.recID)
-    def printStatus(self):
+    def print_Status(self):
         lst = self.status()[:]
         lst[0] = codeMaps.outputStatus1.get(lst[0], lst[0])
         print(f"{self.name}: {self.statusKey}{self.status()} - {lst}")
 
 # subclass for trouble input objects (inherits from wxIO)
-class TroubleInput(WxIO):
+class Trouble_Input(WxIO):
     def __init__(self, name, recID, sessid):
         super().__init__(name, recID, sessid)
         self.statusKey = "TroubleInput" + str(self.recID)
-    def printStatus(self):
+    def print_Status(self):
         lst = self.status()[:]
         lst[0] = codeMaps.troubleInputStatus1.get(lst[0], lst[0])
         binary_str = bin(lst[1])[2:].rjust(2, '0')
@@ -131,7 +131,7 @@ class TroubleInput(WxIO):
 
 ## FUNCTIONS ##
 # generate a random 32 character string
-def generateSessionID():
+def generate_Session_ID():
     sessid = ""
     for i in range(32):
         tval = format(random.randint(0, 15), 'x')
@@ -139,14 +139,14 @@ def generateSessionID():
     return sessid
 
 # run a query against the API using the given login parameters
-def runLoginQuery(url, queryParams, sessid):
+def run_Login_Query(url, queryParams, sessid):
     queryParams += f"&SessionID={sessid}"
-    url_params = url + queryParams
-    response = urllib.request.urlopen(url_params).read().decode('utf-8')
+    urlParams = url + queryParams
+    response = urllib.request.urlopen(urlParams).read().decode('utf-8')
     return response
 
 # xor a string with a number
-def xorFn(pswd: str, num: int):
+def xor_Fn(pswd: str, num: int):
     numbin = format(num, '032b') # convert number to binary string
     startpos = len(numbin) # start at end of the string (right hand side)
     retval = ""
@@ -163,22 +163,22 @@ def xorFn(pswd: str, num: int):
 # login to the WX API and initiate a session encrypted over HTTPS
 def login(url):
     global SEQUENCE
-    sessid = generateSessionID()
+    sessid = generate_Session_ID()
     pswdHash = sha1(secrets.password.encode('utf-8')).hexdigest()
-    data = runLoginQuery(url, params.init, sessid) # initiate session
-    xorUsername = xorFn(secrets.username, (int(data) +1))
+    data = run_Login_Query(url, params.init, sessid) # initiate session
+    xorUsername = xor_Fn(secrets.username, (int(data) +1))
     hashXorUsername = sha1(xorUsername.encode('utf-8')).hexdigest().upper()
-    xorPswdHash = xorFn(pswdHash, int(data))
+    xorPswdHash = xor_Fn(pswdHash, int(data))
     hashXorPswdHash = sha1(xorPswdHash.encode('utf-8')).hexdigest().upper()
     loginParams = f"{params.login}&Name={hashXorUsername}&Password={hashXorPswdHash}" # build the login parameters
-    data = runLoginQuery(url, loginParams, sessid) # login to session
+    data = run_Login_Query(url, loginParams, sessid) # login to session
     if data.startswith("FAIL"):
         raise ValueError(f"Login failed with error code {data}")
     SEQUENCE = 0 # reset sequence number
     return sessid
 
 #*TODO run a query against the API using the given parameters and session ID
-def runQuery(url, sessid, queryType, subType, recID = None, command = None, data1 = None, data2 = None):
+def run_Query(url, sessid, queryType, subType, recID = None, command = None, data1 = None, data2 = None):
     global SEQUENCE
     queryParams = f"{sessid}{queryType}{subType}"
     if recID is not None:
@@ -190,15 +190,15 @@ def runQuery(url, sessid, queryType, subType, recID = None, command = None, data
     if data2 is not None:
         queryParams += f"&Data2={data2}"
     queryParams += f"&Sequence={SEQUENCE}"
-    url_params = url + queryParams
-    response = urllib.request.urlopen(url_params).read().decode('utf-8')
+    urlParams = url + queryParams
+    response = urllib.request.urlopen(urlParams).read().decode('utf-8')
     SEQUENCE += 1 # increment sequence number
     if queryType == params.getList:
-        return convertToDict(response)
+        return convert_To_Dict(response)
     return response
 
 # convert a string of key-value pairs to a dictionary
-def convertToDict(inputString):
+def convert_To_Dict(inputString):
     dictionary = {}
     for item in inputString.split("&"):
         key, value = item.split("=")
@@ -206,13 +206,13 @@ def convertToDict(inputString):
     return dictionary
 
 # build a dictionary of status codes for areas, doors, inputs, outputs, and trouble inputs
-def updateStatusDict(url, sessid):
+def update_Status_Dict(url, sessid):
     global STATUS_DICT
-    areaStatusList = runQuery(url, sessid, params.getStatus, params.areas)
-    doorStatusList = runQuery(url, sessid, params.getStatus, params.doors)
-    inputStatusList = runQuery(url, sessid, params.getStatus, params.inputs)
-    outputStatusList = runQuery(url, sessid, params.getStatus, params.outputs)
-    troubleInputStatusList = runQuery(url, sessid, params.getStatus, params.troubleInputs)
+    areaStatusList = run_Query(url, sessid, params.getStatus, params.areas)
+    doorStatusList = run_Query(url, sessid, params.getStatus, params.doors)
+    inputStatusList = run_Query(url, sessid, params.getStatus, params.inputs)
+    outputStatusList = run_Query(url, sessid, params.getStatus, params.outputs)
+    troubleInputStatusList = run_Query(url, sessid, params.getStatus, params.troubleInputs)
     for i, lst in enumerate([areaStatusList, doorStatusList, inputStatusList, outputStatusList, troubleInputStatusList]):
         tempDict = {}
         statusList = lst.split('&')
@@ -225,7 +225,7 @@ def updateStatusDict(url, sessid):
         STATUS_DICT.update(tempDict)
 
 # build a dictionary of objects from a dictionary of key-value pairs
-def buildObjectList(ioList, name, sessid):
+def build_Object_List(ioList, name, sessid):
     switch = {
         "Area": [],
         "Door": [],
@@ -244,7 +244,7 @@ def buildObjectList(ioList, name, sessid):
         elif name == "Output":
             value = Output(value, key, sessid)
         elif name == "TroubleInput":
-            value = TroubleInput(value, key, sessid)
+            value = Trouble_Input(value, key, sessid)
         else:
             raise ValueError("Invalid object type")
         objectList.append(value)
@@ -256,14 +256,14 @@ def main():
 
     # initiate session and build status dictionary
     sessionID = login(url)
-    updateStatusDict(url, sessionID)
+    update_Status_Dict(url, sessionID)
 
     # build objects
-    areas = buildObjectList(runQuery(url, sessionID, params.getList, params.areas), "Area", sessionID)
-    doors = buildObjectList(runQuery(url, sessionID, params.getList, params.doors), "Door", sessionID)
-    inputs = buildObjectList(runQuery(url, sessionID, params.getList, params.inputs), "Input", sessionID)
-    outputs = buildObjectList(runQuery(url, sessionID, params.getList, params.outputs), "Output", sessionID)
-    troubleInputs = buildObjectList(runQuery(url, sessionID, params.getList, params.troubleInputs), "TroubleInput", sessionID)
+    areas = build_Object_List(run_Query(url, sessionID, params.getList, params.areas), "Area", sessionID)
+    doors = build_Object_List(run_Query(url, sessionID, params.getList, params.doors), "Door", sessionID)
+    inputs = build_Object_List(run_Query(url, sessionID, params.getList, params.inputs), "Input", sessionID)
+    outputs = build_Object_List(run_Query(url, sessionID, params.getList, params.outputs), "Output", sessionID)
+    troubleInputs = build_Object_List(run_Query(url, sessionID, params.getList, params.troubleInputs), "TroubleInput", sessionID)
 
     #* TESTING AREA
     print("\nArea Statuses: ") #TODO remove this line when done testing
